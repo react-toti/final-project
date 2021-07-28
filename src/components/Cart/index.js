97b08e10069c2d6ce814f9e55eca8fd36b390f0a
fjavier02 <franciscolopez1995@gmail.com>
2021-07-28 20:14:17 -0300

    create component cart

---
+++ src/components/Cart/index.js
@@ -0,0 +1,5 @@
+import Grid from './Grid';
+import Modal from './Modal';
+
+
+export {Grid, Modal};
---
+++ src/services/api2.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const api2 = axios.create({
+  baseURL: "https://60ff37602574110017078906.mockapi.io",
+});
+
+export default api2;
