da48b23de84ad11ec2e3d61e14a0ae8cd70d4043
Danilsonunes <danilsonunes@gmail.com>
2021-07-27 22:40:18 -0300

    arreglando formik y pagina de cma

--- tecnoblog/src/Pages/CMA/index.js
+++ tecnoblog/src/Pages/CMA/index.js
@@ -1,125 +1,77 @@
 import React from 'react';
-import * as Yup from 'yup'
-import { Formik } from 'formik';
-import { Text3 } from '../../components/Text/styles';
+import axios from 'axios';
 import api from '../../services/api';
-import * as S from './styles.js';
-import Button from '../../components/Button';
-import Input from '../../components/Input';
+import { Formik } from 'formik';
 
-
-const CMA = props =>{    
-     const initialValues = {
-            name: '',
-            email: '',
-            password: '',
-            confirmPassword: '',
-          };
+const CMA = () => (
+  <div>
+    <h1>Crea Tu Noticia</h1>
+    <Formik
+      initialValues={{ title: '', subTitle: '', article: '', image1: '', image2: '' }}
+      onSubmit={(values, { setSubmitting }) => {
+        api.post('/articles', {
+          title: values.title,
+          subTitle: values.subTitle,
+          article: values.article,
+          image1: values.image1,
+          image2: values.image2,
         
-    const scheme = Yup.object().shape({
-             titulo: Yup.string()
-             .min(2, 'Muito curto')
-             .max(50, 'Muito longo')
-             .required('Nome obrigatório'),
-             email: Yup.string().email('E-mail inválido').required('E-mail obrigatório'),
-             password: Yup.string()
-             .required('Senha obrigatória')
-             .min(6, 'Senha muito curta, precisa ter pelo menos 6 caracteres'),
-             confirmPassword: Yup.string().oneOf(
-             [Yup.ref('password'), null],
-             'Senhas devem ser iguais',
-             ),
-         });
-    const onSubmit = async (values, resetForm) => {
-            const response = await api.post('/users', { values });
-            if (response.status === 201) {
-                resetForm({ values: '' });
-                props.history.push('/home');
-            }
-        };
-            
-        
-    return(
-        <S.CMAContainer> 
-         <Formik
-          initialValues={initialValues}
-          validationSchema={scheme}
-          onSubmit={(values, { resetForm }) => onSubmit(values, resetForm)}
-        >
-          <form>
-            <Input id="titulo" name="name" type="text" placeholder="Titulo da materia" />
-            <Input name="subtitulo" type="text" placeholder="Subtitulo da materia" />
-            <Input name="img" type="text" placeholder="Url primera imagen" />
-            <Input
-              name="segundaImg"
-              type="text"
-              placeholder="Url de la segunda imagen"
-            />
-            <Button type="submit">Publicar</Button>
-            <Text3 color="blue">ya veremos que podra ser.</Text3>
-          </form>
-        </Formik>
-        </S.CMAContainer>
-    )
-}
+    })
+    .then(function (response) {
+      if (response.status === 201) {
+        setSubmitting(false);
+        console.log('Success!');
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+      }}
+    >
+      {({
+        values,
+        handleChange,
+        handleSubmit,
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="string"
+            name="title"
+            onChange={handleChange}
+            value={values.title}
+          />
+          <input
+          type="string"
+          name="subTitle"
+          onChange={handleChange}
+          value={values.subTitle}
+        />
+        <input
+        type="string"
+        onChange={handleChange}
+        name="article"
+        value={values.article}
+      />
+      <input
+      type="string"
+      name="image1"
+      onChange={handleChange}
+      value={values.image1}
+    />
+          <input
+            type="string"
+            name="image2"
+            onChange={handleChange}
+            value={values.image2}
+          />
+          
+          <button type="submit" >
+            Submit
+          </button>
+        </form>
+      )}
+    </Formik>
+  </div>
+);
 
 export default CMA;
-
-// const initialValues = {
-//     name: '',
-//     email: '',
-//     password: '',
-//     confirmPassword: '',
-//   };
-
-//   const scheme = Yup.object().shape({
-//     name: Yup.string()
-//       .min(2, 'Muito curto')
-//       .max(50, 'Muito longo')
-//       .required('Nome obrigatório'),
-//     email: Yup.string().email('E-mail inválido').required('E-mail obrigatório'),
-//     password: Yup.string()
-//       .required('Senha obrigatória')
-//       .min(6, 'Senha muito curta, precisa ter pelo menos 6 caracteres'),
-//     confirmPassword: Yup.string().oneOf(
-//       [Yup.ref('password'), null],
-//       'Senhas devem ser iguais',
-//     ),
-//   });
-
-//   const onSubmit = async (values, resetForm) => {
-//     const response = await api.post('/users', { values });
-//     if (response.status === 201) {
-//       resetForm({ values: '' });
-//       props.history.push('/dashboard');
-//     }
-//   };
-
-//   return (
-//     <>
-//       <Container>
-//         {/* <PhrasesContainer>
-//         <img src={connectedWorld} alt="Mundo conectado" />
-//       </PhrasesContainer> */}
-//         <Formik
-//           initialValues={initialValues}
-//           validationSchema={scheme}
-//           onSubmit={(values, { resetForm }) => onSubmit(values, resetForm)}
-//         >
-//           <FormContainer>
-//             <Input id="name" name="name" type="text" placeholder="Nome" />
-//             <Input name="email" type="email" placeholder="Email" />
-//             <Input name="password" type="password" placeholder="Senha" />
-//             <Input
-//               name="confirmPassword"
-//               type="password"
-//               placeholder="Confirmação de senha"
-//             />
-//             <Button type="submit">Cadastrar</Button>
-//             <Text3 color="blue">Já tenho conta. Entrar.</Text3>
-//           </FormContainer>
-//         </Formik>
-//       </Container>
-//     </>
-//   );
-// };
