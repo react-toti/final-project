8183b5ef5cf29b0494bd7780e3605130bc079981
fjavier02 <franciscolopez1995@gmail.com>
2021-07-28 22:22:07 -0300

    create List and checkout for component Cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Grid from '../../components/Cart/components/Grid';
 
 
 const Cart = (props) => {
   return (
     <>
         <h1>Cart</h1>
+        <Grid></Grid>
     </>
   );
 };
